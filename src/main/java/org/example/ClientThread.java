61b38500e6c0058a172d56e3b68f5293842b9bfc
ndrtprv <ndrtprv@gmail.com>
2022-12-12 10:47:21 +0200

    Realized part Server for the pattern Client-Server.

---
+++ src/main/java/org/example/ClientThread.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientThread implements Runnable{
+    private final Socket clientSocket;
+    private final ChatServer chatServer;
+    private final int numClient;
+
+    public ClientThread(Socket clientSocket,
+                        ChatServer chatServer, int numClient) {
+        this.clientSocket = clientSocket;
+        this.chatServer = chatServer;
+        this.numClient = numClient;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            System.out.println("Client " + numClient + " has been connected.");
+
+            new PrintWriter(clientSocket.getOutputStream(), true).println("Client " + numClient);
+
+            String clientMessage;
+            while (true) {
+                clientMessage = in.readLine();
+                if (!"exit".equalsIgnoreCase(clientMessage)) {
+                    System.out.println("Client " + numClient + " has sent message: " + clientMessage);
+                    chatServer.sendMessageForAllClient(numClient, clientMessage);
+                } else {
+                    System.out.println("Client " + numClient + " has been disconnected.");
+                    chatServer.clientExitedChat(numClient);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR!");
+            System.out.println(e.getMessage());
+        }
+    }
+}
